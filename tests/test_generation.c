fb9c34dad846c9266b6e810218dc69982086bef3
Martin <malonso@satellogic.com>
2018-01-08 00:53:12 -0300

    Add generation tests

--- tests/test_generation.c
+++ tests/test_generation.c
@@ -12,4 +12,26 @@ TEST(test_node_init)
     rv = node_init(&node);
 
     assertEquals(rv, RV_SUCCESS);
+    assertEquals(node.value, 0);
+    assertEquals(node.turn, WHITE);
+    assertEquals(node.check_status, NOT_CHECK);
+    assertEquals(node.castles, ALL_CASTLES);
+    assertEquals(node.child, NULL);
+    assertEquals(node.next, NULL);
+    assertEquals(node.parent, NULL);
+    
+    int i,j;
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            assertEquals(node.board[i][j], initial_board[i][j]);
+        }
+    }
+}
+
+TEST(test_node_initi_fail)
+{
+    retval_t rv;
+
+    rv = node_init(NULL);
+    assertEquals(rv, RV_ERROR);
 }
--- tests/test_generation.h
+++ tests/test_generation.h
@@ -1,10 +1,12 @@
 #include "cu/cu.h"
 
-  // Declarations of tests
-  TEST(test_node_init);
+// Declarations of tests
+TEST(test_node_init);
+TEST(test_node_initi_fail);
 
-  // Collect tests into test suite
-  TEST_SUITE(generation) {
-      TEST_ADD(test_node_init),
-      TEST_SUITE_CLOSURE
-  };
+// Collect tests into test suite
+TEST_SUITE(generation) {
+    TEST_ADD(test_node_init),
+    TEST_ADD(test_node_initi_fail),
+    TEST_SUITE_CLOSURE
+};
