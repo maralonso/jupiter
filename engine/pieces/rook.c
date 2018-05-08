19dd05465faaaa6e5f9625bf84da4b62f874f216
Martin <malonso@satellogic.com>
2018-05-08 18:13:17 -0300

    Implement rook avaluation

--- engine/pieces/rook.c
+++ engine/pieces/rook.c
@@ -1,6 +1,7 @@
 #include "board.h"
 #include "pieces.h"
 #include "generation.h"
+#include "evaluation.h"
 
 static retval_t rook_moves(Node_t *node, square sq, uint8_t file, uint8_t col)
 {
@@ -26,7 +27,7 @@ retval_t get_rook_moves(Node_t *node, square sq)
     return exec_with_rotation(node, sq, rotation, rook_moves); 
 }
 
-bool rook_attak_square(Node_t *node, square from, square to)
+bool rook_attak_square(Board board, square from, square to)
 {
     int8_t rank_step =0;
     int8_t file_step =0;
@@ -42,7 +43,7 @@ bool rook_attak_square(Node_t *node, square from, square to)
         }
         for (int8_t i = from[0] + file_step, j = from[1] + rank_step;
              i != to[0] || j != to[1]; i += file_step, j += rank_step) {
-            if (node->board[i][j] != 0) {
+            if (board[i][j] != 0) {
                 return false;
             }
         }   
@@ -51,8 +52,89 @@ bool rook_attak_square(Node_t *node, square from, square to)
     return false;
 }
 
+static bool connected_rooks(Board board, uint8_t file, uint8_t rank)
+{
+    int16_t rook = board[file][rank];
+
+    for (int i = FILE_1; i < FILE_8; i++) {
+        if (board[i][rank] == rook && i != file) {
+            return true;
+        }
+    }
+
+    for (int i = COL_A; i < COL_H; i++) {
+        if (board[file][i] == rook && i != rank) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+static int8_t available_rook_moves(Board board, uint8_t file, uint8_t rank)
+{
+    int8_t count = 0;
+    square from = {file, rank};
+
+    for (int i = FILE_1; i < FILE_8; i++) {
+        square to = {i, rank};
+
+        if (rook_attak_square(board, from, to)) {
+            count++;
+        }
+    }
+   
+    for (int i = COL_A; i < COL_H; i++) {
+        square to = {file, i};
+
+        if (rook_attak_square(board, from, to)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+static bool x_ray(Board board, uint8_t file, uint8_t rank)
+{
+    int8_t not_turn = NOT_TURN(board, file, rank);
+    square from = {file, rank};
+
+    for (int i = FILE_1; i < FILE_8; i++) {
+        square to = {i, rank};
+        if ((board[i][rank] == (QUEEN * not_turn) || 
+            board[i][rank] == (KING * not_turn)) &&
+            (!(rook_attak_square(board, from, to)))) {
+            return true;
+        }
+    }
+
+    for (int i = COL_A; i < COL_H; i++) {
+        square to = {file, i};
+        if ((board[file][i] == (QUEEN * not_turn) || 
+            board[file][i] == (KING * not_turn)) &&
+            !((rook_attak_square(board, from, to)))) {
+            return true;
+        }
+    }
+        
+    return false;
+}
+
 int32_t rook_evaluation(Board board, uint8_t file, uint8_t rank)
 {
-    //TODO
-    return 0;
+    int32_t eval = 0;
+    int8_t turn = TURN(board, file, rank);
+
+    eval += available_rook_moves(board, file, rank);
+
+    if (connected_rooks(board, file, rank)) {
+            eval += CONNECTED_ROOKS_POND;
+    }
+
+    if (x_ray(board, file, rank)) {
+        eval += ROOK_X_RAY_POND;
+    }
+
+    return eval * turn;
 }
