deeeb5fa080b57633791f9c27c20cd6a6671a699
Martin <malonso@satellogic.com>
2018-05-19 19:10:26 -0300

    Add missing include

--- engine/fen.h
+++ engine/fen.h
@@ -1,8 +1,9 @@
 #ifndef _FEN_H_
 #define _FEN_H_
 
-#define MAX_FEN_LEN (sizeof("rnbkqbnr/pppppppp/00000000/00000000/ \
-            00000000/00000000/PPPPPPPP/RNBKQBNR w KQwq e3 20 100 "))
+#include "board.h"
+
+#define MAX_FEN_LEN (sizeof("rnbkqbnr/pppppppp/00000000/00000000/00000000/00000000/PPPPPPPP/RNBKQBNR w KQwq e3 20 100 "))
 
 retval_t get_fen_from_node(Node_t *node, char *fen);
 retval_t get_node_from_fen(Node_t *node, char *fen);
