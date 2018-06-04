e3289d663d0ec6b108d0537d137ee52ccdc14039
Martin <malonso@satellogic.com>
2018-06-04 18:01:38 -0300

    Add search functions

---
+++ engine/search.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include "notation.h"
+#include "board.h"
+
+static int32_t get_minimax(Node_t *node)
+{
+    Node_t *aux = node->child;
+    int32_t minmax = -10000 * node->turn;
+
+    while (aux != NULL) {
+        if ((aux->value * node->turn) > minmax) {
+            minmax = aux->value;
+        }
+        aux = aux->next;
+    }
+
+    return minmax;
+}
+
+static void search(Node_t *root)
+{
+    Node_t *aux = root;
+
+    while(aux != NULL) {
+        if (aux->child != NULL) {
+            if (aux->child->child == NULL) {
+                aux->value = get_minimax(aux);
+            } else {
+                search(aux->child);
+            }
+        }
+        aux = aux->next;
+    }
+}
+
+static retval_t get_move(Node_t *node, char *move)
+{
+    Node_t *aux = node->child;
+    int32_t score = node->value;
+
+    while (aux->value != score && aux != NULL) {
+        aux = aux->next;
+    }
+
+    if (aux == NULL) {
+        memset(move, 0x0, NOTATION_STR_LEN);
+        return RV_ERROR;
+    }
+    strncpy(move, aux->notation, NOTATION_STR_LEN);
+    return RV_SUCCESS;
+}
+
+void get_best_move(Node_t *node, char *move)
+{
+    search(node);
+    get_move(node, move);
+}
---
+++ engine/search.h
@@ -0,0 +1,7 @@
+#ifndef _SEARCH_H_
+#define _SEARCH_H_
+
+#include "board.h"
+
+void get_best_move(Node_t *node, char *move);
+#endif
